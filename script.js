0e17ee8d78ee3c63f226475e3fbd7c9657d77b11
iugorsette <iugorsette@gmail.com>
2023-03-22 20:31:12 -0300

    fix commit

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let buttonElement = document.querySelector("#button");
 let sizePassword = document.querySelector("#valor");
 let password = document.querySelector("#password");
 
-l
 
 let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#";
 let novaSenha = "";
